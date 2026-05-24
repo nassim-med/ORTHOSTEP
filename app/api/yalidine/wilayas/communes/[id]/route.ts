3e5172b81e9a68a475fd47a920450de45bd8a2db
nassim-med <nssmpro@gmail.com>
2026-05-24 21:49:44 +0100

    Initial commit

--- app/api/yalidine/wilayas/communes/[id]/route.ts
+++ app/api/yalidine/wilayas/communes/[id]/route.ts
@@ -1,27 +1,45 @@
 import { NextResponse } from "next/server";
 
 export async function GET(
-req:Request,
-{params}:{params:{wilaya:string}}
+request: Request,
+{ params }:{
+params:Promise<{
+id:string
+}>
+}
 ){
+
+const {id}=
+await params;
+
+try{
 
 const response=
 await fetch(
-
-`https://api.yalidine.com/v1/communes/?wilaya_id=${params.wilaya}`,
-
+`https://api.yalidine.com/v1/communes/?wilaya_id=${id}`,
 {
 headers:{
-"X-ID":process.env.YALIDINE_API_ID!,
-"X-TOKEN":process.env.YALIDINE_API_TOKEN!
+Authorization:
+process.env.YALIDINE_API!
 }
 }
-
 );
 
 const data=
 await response.json();
 
-return NextResponse.json(data);
+return NextResponse.json(
+data
+);
+
+}catch{
+
+return NextResponse.json(
+{
+data:[]
+}
+)
+
+}
 
 }
